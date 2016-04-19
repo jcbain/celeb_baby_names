2acc152855bfbace4d2cf549b677d2dd8a87b7b0
James Bain <jamescbain@gmail.com>
2016-04-19 12:22:01 -0500

    create subset data frames before and after 1980

--- approval_analysis.R
+++ approval_analysis.R
@@ -1,18 +1,41 @@
 library(ggplot2)
 
+###########################
+## MIN-MAX NORMALIZATION ##
+###########################
 normalize <- function(x) {
   return ((x - min(x)) / (max(x) - min(x)))
 }
 
+#######################
+## DATA MANIPULATION ##
+#######################
 avg <- read.csv('data/presApprovalAverage.csv')
 avg$fname_rate<-avg$f_count/avg$total
 avg$lname_rate<-avg$l_count/avg$total
 avg$norm_appr<-ave( avg$Approving, avg$President, FUN = normalize )
 avg$norm_fname<-ave( avg$fname_rate, avg$President, FUN = normalize )
+avg$norm_lname<-ave( avg$lname_rate, avg$President, FUN = normalize )
+
+pre1981<- subset(avg, year < 1981)
+post1980<-subset(avg,year>1980)
+##############
+## ANALYSIS ##
+##############
+lm(norm_fname~norm_appr+President, data=avg)
+
+
 # sample plot
-ggplot(avg, aes(x = year, y= norm_appr, colour = President)) +  geom_line() #+ facet_grid(.~f_name)
-ggplot(avg, aes(x = year, y= norm_fname, colour = President)) +  geom_line()
+# ggplot(avg, aes(x = year, y= norm_appr, colour = President)) +  geom_line() #+ facet_grid(.~f_name)
+# ggplot(avg, aes(x = year, y= norm_fname, colour = President)) +  geom_line()
+
+ggplot(pre1981,aes(x=norm_appr, y =norm_fname))+geom_line(aes(colour=President)) +
+  geom_point(aes(colour = President)) + facet_wrap(~President)
+
+ggplot(prost1980,aes(x=norm_appr, y =norm_fname))+geom_line(aes(colour=President)) +
+  geom_point(aes(colour = President)) + facet_wrap(~President)
 
 ggplot(avg, aes(year)) + 
-  geom_line(aes(y = norm_appr, colour = President)) + 
-  geom_line(aes(y = norm_fname, colour = President),linetype = 2)+  facet_wrap(~f_name)
+  geom_line(aes(y = norm_appr, colour = President),alpha =.5) + 
+  geom_line(aes(y = norm_fname, colour = President),linetype = 2) +
+  facet_wrap(~President)
